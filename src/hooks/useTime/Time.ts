8f84caa70c1bce395078104bb5b5a496ac781f76
Crgio <randaledomman1@gmail.com>
2021-09-30 16:54:30 -0300

    feat: add time pre config

--- src/hooks/useTime/Time.ts
+++ src/hooks/useTime/Time.ts
@@ -3,6 +3,20 @@ export type Time = {
 	minutes: number;
 };
 
+export type Period = {
+	from: Time;
+	to: Time;
+};
+
+export const TimeConfig = {
+	FromZero: { seconds: 0, minutes: 0 },
+	ToInfinite: { seconds: 0, minutes: Number.POSITIVE_INFINITY },
+	FromZeroToInfinite: {
+		from: { seconds: 0, minutes: 0 },
+		to: { seconds: 0, minutes: Number.POSITIVE_INFINITY },
+	},
+};
+
 export const formatTime = (time: Time): string => {
 	return `${time.minutes < 10 ? '0' + String(time.minutes) : time.minutes}:${
 		time.seconds < 10 ? '0' + String(time.seconds) : time.seconds
