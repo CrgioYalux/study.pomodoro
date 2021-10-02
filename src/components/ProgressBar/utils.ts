994685291b3ec76d04303f0b1110a51f2676484a
Crgio <randaledomman1@gmail.com>
2021-10-02 02:15:23 -0300

    refactor: change of directory the subtraction for Time typeof values util

--- src/components/ProgressBar/utils.ts
+++ src/components/ProgressBar/utils.ts
@@ -1,13 +1,7 @@
-import { secondsToTime, timeToSeconds, Time } from '../../hooks/useTime/Time';
+import { timeToSeconds, Time } from '../../hooks/useTime/Time';
 
 export const timeToPercentage = (internalTime: Time, timer: Time): number => {
 	return (timeToSeconds(timer) * 100) / timeToSeconds(internalTime);
-};
-
-export const timeLeft = (internalTime: Time, timer: Time): Time => {
-	const internalTimeToSeconds = timeToSeconds(internalTime);
-	const timerToSeconds = timeToSeconds(timer);
-	return secondsToTime(internalTimeToSeconds - timerToSeconds);
 };
 
 export const progressInPercentage = (
