456d9a54f7f9cf368a6c34b1d83c64fbf44fc3cb
Crgio <randaledomman1@gmail.com>
2021-10-03 03:56:27 -0300

    feat: update to use sass vars

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -4,6 +4,14 @@ import { useTasksUtil } from '../../providers/TasksUtil';
 import { Task } from '../../hooks/useTasks/Task';
 export const Dashboard = () => {
 	const { tasks } = useTasksUtil();
+
+	if (tasks.length === 0)
+		return (
+			<ul className="Dashboard-container">
+				<li className="Dashboard-notasks">0 tasks</li>
+			</ul>
+		);
+
 	return (
 		<ul className="Dashboard-container">
 			{tasks.map(({ id, title, period, duration }: Task) => (
