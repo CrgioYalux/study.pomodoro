27c9217df9d2c6849a6c494e63c094acdada47b5
Crgio <randaledomman1@gmail.com>
2021-11-03 03:04:09 -0300

    feat: display only actual task / display the number of tasks done

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,11 +1,18 @@
 import './Dashboard.scss';
 import { DisplayTask } from '../DisplayTask';
 import { useTasksUtil } from '../../providers/TasksUtil';
-import { Task } from '../../hooks/useTasks/Task';
 import { DraggableContainer } from '../DraggableContainer';
+import { useTimerUtil } from '../../providers/TimerUtil';
+import { isTaskDone } from '../TaskProgressState/utils';
 
 export const Dashboard = () => {
 	const { tasks } = useTasksUtil();
+	const { time } = useTimerUtil();
+	const tasksDone = tasks.reduce<number>((acc, arr) => {
+		if (isTaskDone(arr, time)) return acc + 1;
+		else return acc + 0;
+	}, 0);
+	const tasksDoneText = `${tasksDone} / ${tasks.length} tasks done`;
 
 	if (tasks.length === 0)
 		return (
@@ -13,12 +20,15 @@ export const Dashboard = () => {
 				<h3 className="Dashboard-notasks">0 tasks</h3>
 			</DraggableContainer>
 		);
-
 	return (
 		<DraggableContainer classNameContainer="Dashboard-container _not_empty">
-			{tasks.map((task: Task) => (
-				<DisplayTask key={task.id} task={task} />
-			))}
+			<DisplayTask
+				task={
+					tasks.find((task) => !isTaskDone(task, time)) ||
+					tasks[tasks.length - 1]
+				}
+			/>
+			<strong className="TasksDone">{tasksDoneText}</strong>
 		</DraggableContainer>
 	);
 };
